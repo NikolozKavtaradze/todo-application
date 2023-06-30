819cc6a78359fdd90911d097fadf97bc80ecd1bd
Nikoloz Kavtaradze <nikoloz.kavtaradze8@gmail.com>
2023-06-30 12:13:00 +0400

    Required for list name

--- todo-domain-entities/AggregateModels/ToDoList.cs
+++ todo-domain-entities/AggregateModels/ToDoList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace todo_domain_entities.AggregateModels
@@ -7,6 +8,8 @@ namespace todo_domain_entities.AggregateModels
     public class ToDoList
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public virtual List<ToDoItem> ToDoItems { get; set; }
 
